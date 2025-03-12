78793fe6e1115374732db78007a4b2b6e2b4c9b3
IscoRuta98 <fmruta98@gmail.com>
2025-03-12 16:43:43 +0200

    fix: ammend typos

--- Tutorial_3/students/10_export_data_to_csv.py
+++ Tutorial_3/students/10_export_data_to_csv.py
@@ -2,20 +2,20 @@ import sqlite3
 import csv
 
 
-def export_to_csv(file_name):
+def export_to_csv(file_name: str):
     """Export data from the Students table to a CSV file."""
     with sqlite3.connect("my_database.db") as connection:
         cursor = connection.cursor()
 
         # Execute a query to fetch all customer data
         cursor.execute("SELECT * FROM Students;")
-        customers = cursor.fetchall()
+        students = cursor.fetchall()
 
         # Write data to CSV
         with open(file_name, "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(["ID", "Name", "Age", "Email"])  # Writing header
-            csv_writer.writerows(customers)  # Writing data rows
+            csv_writer.writerows(students)  # Writing data rows
 
         print(f"Data exported successfully to {file_name}.")
 
